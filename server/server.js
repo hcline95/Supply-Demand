bf1bcc3b7f8c90424626f987fbe68e0b464bca84
Hannah Cline <hannahcline@Hannahs-MacBook-Air.local>
2020-04-07 17:56:52 -0400

    m

--- server/server.js
+++ server/server.js
@@ -8,6 +8,19 @@ const port = process.env.PORT || 8000;
 //connects SQL database
 var pool  = mysql.createPool(keys.CLEARDB_DATABASE_URL);
 
+if (process.env.NODE_ENV === 'production') {
+    // Express will serve up production assets
+    // like our main.js file, or main.css file!
+    app.use(express.static('client/build'));
+  
+    // Express will serve up the index.html file
+    // if it doesn't recognize the route
+    const path = require('path');
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+  }
+
 //sets up socket
 // const io = require('socket.io')(server, {
 //   transports: ['websocket', 'polling']
