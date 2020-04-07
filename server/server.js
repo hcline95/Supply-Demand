359a66875fdc38bbb78f939526946031c635056c
Hannah Cline <hannahcline@Hannahs-MacBook-Air.local>
2020-04-07 17:33:48 -0400

    m

--- server/server.js
+++ server/server.js
@@ -9,86 +9,86 @@ const port = process.env.PORT || 8000;
 var pool  = mysql.createPool(keys.CLEARDB_DATABASE_URL);
 
 //sets up socket
-const io = require('socket.io')(server, {
-  transports: ['websocket', 'polling']
-});
+// const io = require('socket.io')(server, {
+//   transports: ['websocket', 'polling']
+// });
 
 
 
-//connects to socket
-io.on('connection', client => {
-    console.log('Client connected...'); 
-    //waits for click on client side to start sending intervals back
-    client.on('myClick', (data) => {
+// //connects to socket
+// io.on('connection', client => {
+//     console.log('Client connected...'); 
+//     //waits for click on client side to start sending intervals back
+//     client.on('myClick', (data) => {
        
-        //returns the the table from supply and demand
-        pool.query('SELECT * FROM Supply_Demand', function (error, results, fields) {
-            // error will be an Error if one occurred during the query
-            if (error) throw error;
+//         //returns the the table from supply and demand
+//         pool.query('SELECT * FROM Supply_Demand', function (error, results, fields) {
+//             // error will be an Error if one occurred during the query
+//             if (error) throw error;
 
-            results.map((row, i)=>{
-                 setTimeout(() => {
+//             results.map((row, i)=>{
+//                  setTimeout(() => {
 
-                    console.log(row.stage)
-                    //returns suppliers table where the stage ID = to the row.stage id
-                    pool.query(`SELECT * FROM suppliers where stage = ${row.stage}`, function (error, suppliers, fields) {
-                    // error will be an Error if one occurred during the query
-                    if (error) throw error;
+//                     console.log(row.stage)
+//                     //returns suppliers table where the stage ID = to the row.stage id
+//                     pool.query(`SELECT * FROM suppliers where stage = ${row.stage}`, function (error, suppliers, fields) {
+//                     // error will be an Error if one occurred during the query
+//                     if (error) throw error;
 
-                    io.emit('suppliers', suppliers)
-                    })
+//                     io.emit('suppliers', suppliers)
+//                     })
 
-                    io.emit('stage', row.stage)
+//                     io.emit('stage', row.stage)
 
-                    io.emit('diary', {day: `Day ${row.stage * 2}`, price:row.price, quantity:row.quantity, description:row.description})
+//                     io.emit('diary', {day: `Day ${row.stage * 2}`, price:row.price, quantity:row.quantity, description:row.description})
                 
-                    io.emit('quantity', {Quantity: row.quantity})
-                    //price graph
+//                     io.emit('quantity', {Quantity: row.quantity})
+//                     //price graph
                    
-                    io.emit('number', {name:`Day ${row.stage * 2}`, value:row.price})
+//                     io.emit('number', {name:`Day ${row.stage * 2}`, value:row.price})
 
-                    //main supply and demand graph
-                    io.emit('mainGraph', {demand:[{x:row.demand1x, y:row.demand1y}, {x:row.demand2x, y:row.demand2y}], supply:[{x:row.supply1x, y:row.supply1y}, {x:row.supply2x, y:row.supply2y}], equalibrium: row.equal})
+//                     //main supply and demand graph
+//                     io.emit('mainGraph', {demand:[{x:row.demand1x, y:row.demand1y}, {x:row.demand2x, y:row.demand2y}], supply:[{x:row.supply1x, y:row.supply1y}, {x:row.supply2x, y:row.supply2y}], equalibrium: row.equal})
                     
 
-                }, i * 2000);
-            });
-        });
-    })
-})
+//                 }, i * 2000);
+//             });
+//         });
+//     })
+// })
 
 
-io.on('connection', client => {
-    client.on('myClickRandom', (data) => {
-        console.log('clicked')
+// io.on('connection', client => {
+//     client.on('myClickRandom', (data) => {
+//         console.log('clicked')
   
-    pool.query('SELECT * FROM random', function (error, results, fields) {
-        // error will be an Error if one occurred during the query
-        if (error) throw error;
+//     pool.query('SELECT * FROM random', function (error, results, fields) {
+//         // error will be an Error if one occurred during the query
+//         if (error) throw error;
       
 
-        results.map((row, i)=>{
-             setTimeout(() => {
-                 console.log(row.stage)
-                io.emit('random', row )
+//         results.map((row, i)=>{
+//              setTimeout(() => {
+//                  console.log(row.stage)
+//                 io.emit('random', row )
 
-                io.emit('diary', {day: `Day ${row.stage}`, price:row.price, quantity:row.quantity, description:row.description})
+//                 io.emit('diary', {day: `Day ${row.stage}`, price:row.price, quantity:row.quantity, description:row.description})
     
-                io.emit('quantity', {Quantity: row.quantity})
-                //price graph
+//                 io.emit('quantity', {Quantity: row.quantity})
+//                 //price graph
                
-                io.emit('number', {name:`Day ${row.stage}`, value:row.price})
+//                 io.emit('number', {name:`Day ${row.stage}`, value:row.price})
 
-                //main supply and demand graph
-                io.emit('mainGraph', {demand:[{x:row.demand1x, y:row.demand1y}, {x:row.demand2x, y:row.demand2y}], supply:[{x:row.supply1x, y:row.supply1y}, {x:row.supply2x, y:row.supply2y}]})
+//                 //main supply and demand graph
+//                 io.emit('mainGraph', {demand:[{x:row.demand1x, y:row.demand1y}, {x:row.demand2x, y:row.demand2y}], supply:[{x:row.supply1x, y:row.supply1y}, {x:row.supply2x, y:row.supply2y}]})
 
-            }, i * 2000);
-        });
-    });
+//             }, i * 2000);
+//         });
+//     });
  
-})
+// })
 
-})
+// })
 
 
 server.listen(port);
